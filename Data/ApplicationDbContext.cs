94af4eb91c0b7187820374ce1335178586d3101c
BenArbiaAhmed <hamoudabenarbia15@gmail.com>
2026-01-29 22:25:38 +0100

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace tp_5.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
